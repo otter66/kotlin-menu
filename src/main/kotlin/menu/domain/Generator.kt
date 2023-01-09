77c81ef6159477830bd0268a6617b64a6f1c8f35
otter <a33397397@gmail.com>
2023-01-09 23:20:15 +0900

    refactor: 테스트 오류 발생 지점 파악, 주석 추가

--- src/main/kotlin/menu/domain/Generator.kt
+++ src/main/kotlin/menu/domain/Generator.kt
@@ -29,7 +29,13 @@ class Generator {
         var categoryIndex = 0
 
         while (categoryIndex < categories.size) {
-            val randomMenu = Randoms.shuffle(menuBoard.getCategoryMenus(categories[categoryIndex]))[0]
+            val categoryMenus: List<String> = menuBoard.getCategoryMenus(categories[categoryIndex])
+            // todo 테스트 오류 발생 지점
+            // todo 카테고리는 정상적으로 불러오지만,
+            // todo 두번째 돌 때 아래 셔플에서 양식 카테고리의 메뉴를 줘도, 첫번째 카테고리인 한식 카테고리에서 메뉴를 가져옴;;;;;;
+
+            val randomCategoryMenus = Randoms.shuffle(categoryMenus)
+            val randomMenu: String = randomCategoryMenus[0]
 
             if (!menuRecommendations.contains(randomMenu) &&
                 !coach.containsCantEatMenu(randomMenu)
